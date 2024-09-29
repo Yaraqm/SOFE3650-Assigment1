cca4137609d9d1da85471763bd02057b9328769a
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:12:49 -0400

    Update Carrot.java

--- SourceFiles/Carrot.java
+++ SourceFiles/Carrot.java
@@ -1,3 +1,12 @@
+/*
+ * File: Carrot.java
+ * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Date: 2024-09-29
+ * 
+ * Description:
+ * This class represents a Carrot that implements the Vegetable interface.
+ */
+
 public class Carrot implements Vegetable {
     private double price;
 
