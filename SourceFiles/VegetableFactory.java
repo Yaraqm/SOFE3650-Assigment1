6b11944e05dc58c530624874f45b96ac50be9e99
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:14:39 -0400

    Update VegetableFactory.java

--- SourceFiles/VegetableFactory.java
+++ SourceFiles/VegetableFactory.java
@@ -1,11 +1,4 @@
-/*
- * File: Vegetable.java
- * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
- * Date: 2024-09-29
- * 
- * Description:
- * Interface representing a vegetable with a method to get its price.
- */
+
 
 public class VegetableFactory implements GroceryProductFactory {
     @Override
