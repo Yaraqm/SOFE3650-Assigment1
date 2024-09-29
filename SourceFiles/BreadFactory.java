3e0a51d9db97b78d81647549a86a36a12fa61f42
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:18:30 -0400

    Update BreadFactory.java

--- SourceFiles/BreadFactory.java
+++ SourceFiles/BreadFactory.java
@@ -1,6 +1,8 @@
 /*
  * File: BreadFactory.java
- * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Authors: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Lab CRN: 43963
+ * Group Number: 5
  * Date: 2024-09-29
  * 
  * Description:
