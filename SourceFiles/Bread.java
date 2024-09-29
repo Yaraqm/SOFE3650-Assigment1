e86c88ed428f1fd60ba59e26fe14c676306f9614
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:11:10 -0400

    Update Bread.java

--- SourceFiles/Bread.java
+++ SourceFiles/Bread.java
@@ -1,3 +1,12 @@
+/*
+ * File: Bread.java
+ * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Date: 2024-09-29
+ * 
+ * Description: 
+ * Interface representing a type of bread with a method to get its price.
+ */
+
 public interface Bread {
     double getPrice();
 }
