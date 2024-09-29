309a5a410e41903dbeee242e65317ad46ae31c3e
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:11:52 -0400

    Update BreadFactory.java

--- SourceFiles/BreadFactory.java
+++ SourceFiles/BreadFactory.java
@@ -1,3 +1,14 @@
+/*
+ * File: BreadFactory.java
+ * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Date: 2024-09-29
+ * 
+ * Description:
+ * Factory class for creating Bread products. Currently, it creates Bagels by default.
+ * Does not support creating vegetables.
+ */
+
+
 public class BreadFactory implements GroceryProductFactory {
     @Override
     public Bread createBread(double price) {
