8c4659967c2f2adc7a46556be602518625fd5dba
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:13:32 -0400

    Update GroceryProductFactory.java

--- SourceFiles/GroceryProductFactory.java
+++ SourceFiles/GroceryProductFactory.java
@@ -1,3 +1,12 @@
+/*
+ * File: GroceryProductFactory.java
+ * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Date: 2024-09-29
+ * 
+ * Description:
+ * Interface for creating grocery products such as vegetables and bread.
+ */
+
 public interface GroceryProductFactory {
     Vegetable createVegetable(double price);
     Bread createBread(double price);
