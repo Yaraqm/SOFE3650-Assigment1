e8d1f494f05e911903f2771ea24b4c70737e43e7
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:14:49 -0400

    Update Vegetable.java

--- SourceFiles/Vegetable.java
+++ SourceFiles/Vegetable.java
@@ -1,3 +1,12 @@
+/*
+ * File: Vegetable.java
+ * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Date: 2024-09-29
+ * 
+ * Description:
+ * Interface representing a vegetable with a method to get its price.
+ */
+
 public interface Vegetable {
     double getPrice();
 }
