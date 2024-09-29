92835240064a6eefe53c9816d69d07182b16307b
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:10:04 -0400

    Update Bagel.java

--- SourceFiles/Bagel.java
+++ SourceFiles/Bagel.java
@@ -1,3 +1,15 @@
+/*
+ * File: Bagel.java
+ * Author: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
+ * Date: 2024-09-29
+ * 
+ * Description: 
+ * This class represents a Bagel that implements the Bread interface. 
+ * It includes the price of the Bagel and provides an implementation of 
+ * the getPrice() method to retrieve the price.
+ */
+
+
 public class Bagel implements Bread {
     private double price;
 
