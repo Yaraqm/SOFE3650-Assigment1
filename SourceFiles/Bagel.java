004a851e1de6f303111ce4b9fb671b2521383c41
Rushdaaa <138223044+Rushdaaa@users.noreply.github.com>
2024-09-29 16:17:57 -0400

    Update Bagel.java

--- SourceFiles/Bagel.java
+++ SourceFiles/Bagel.java
@@ -2,6 +2,7 @@
  * File: Bagel.java
  * Authors: Yara Mahmoud (100851246), Zainab Ahmed (100868502), Rushda Khan (100865307)
  * Lab CRN: 43963
+ * Group Number: 5
  * Date: 2024-09-29
  * 
  * Description: 
